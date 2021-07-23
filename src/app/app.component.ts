5911173b047a8b0c302cc8498aaa602697bf5360
Facundo Argüello <facuatolosa@gmail.com>
2021-07-23 19:56:27 -0300

    Copia proyecto 'Dominios'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'taller-frontend';
+	desarrollador = 'Argüello,Facundo - Quinteros, José';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InterceptorHttpService } from './services/interceptor-http.service';
+import { appRoutingModule } from './rutas/rutas-routing.module';
+import { InicioComponent } from './inicio/inicio.component';
+import { DominioComponent } from './dominio/dominio.component';
+import { DominioNuevoComponent } from './dominio/dominio-nuevo/dominio-nuevo.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+		AppComponent,
+	    HeaderComponent,
+	    FooterComponent,
+	    LoginComponent,
+	    InicioComponent,
+	    DominioComponent,
+	    DominioNuevoComponent,
   ],
   imports: [
-    BrowserModule
+	  BrowserModule,
+	  ReactiveFormsModule,
+	  HttpClientModule,
+	  appRoutingModule
   ],
-  providers: [],
+	providers: [
+	  { provide: HTTP_INTERCEPTORS, useClass : InterceptorHttpService, multi : true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
